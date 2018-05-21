7e220046f6195261919280568b3ed725ada9a787
Linda Le <linchowmein@hotmail.com>
2018-05-21 17:14:12 +0100

    wrote function to check for bear

--- js/app.js
+++ js/app.js
@@ -3,25 +3,29 @@ window.addEventListener('DOMContentLoaded', () => {
   //instructions button
   //health bar
   //timer
+  //turn into smooth based tile game
+  //add score
+  // add logic
 
 
-  //VARIABLES
+  //GLOBAL VARIABLES
 
   const gridHeight = 10;
   const gridWidth = 10;
   const cells = [];
   const trees = [7,8,9,20,21,22,34,38,44,48,54,61,67,71,77,81,87];
   const grid = document.querySelector('#grid');
-  // const $lumberjackIndex= $('#lumberjackIndex');
-  // const $enemyBear = $('#enemyBear');
+  const lifebar = document.querySelector('#lifebar');
   let lumberjackIndex = 0;
   let bearIndex = 99;
   let pineconeIndex = null;
   let inventory = 5;
+  let score = 10;
+  let life =3;
   // let computerPlayer =false;
 
   // 1. create grid
-
+  //Rendering static tilemaps done with a nested loop iterating over columns and rows.
   function startGame(){
     for (let i=0; i<gridHeight*gridWidth; i++) {
       const div = document.createElement('div');
@@ -30,6 +34,9 @@ window.addEventListener('DOMContentLoaded', () => {
       cells.push(div);
     }
   }
+
+
+  //pro
   startGame();
 
   //add lumberjackIndex to grid
@@ -46,8 +53,17 @@ window.addEventListener('DOMContentLoaded', () => {
   }
   generateTrees();
 
+  function checkBear() {
+    if(lumberjackIndex === bearIndex) console.log('RAWR');{
+      score--;
+      life--;
+      console.log(score);
+    }
+  }
+
   //ARROW BINDING
   window.addEventListener('keydown', (e) => {
+
     //picking up pinecone and respawn
     if (cells[lumberjackIndex].classList.contains('pinecone')){
       cells[lumberjackIndex].classList.remove('pinecone');
@@ -65,22 +81,24 @@ window.addEventListener('DOMContentLoaded', () => {
         lumberjackIndex -= 1;
         cells[lumberjackIndex].classList.add('lumberjack');
       }
+      checkBear();
     }
     if (e.keyCode === 39 ){
-      console.log('right');
       if(lumberjackIndex%gridWidth !== gridWidth-1 && !cells[lumberjackIndex+1].classList.contains('tree')){
         cells[lumberjackIndex].classList.remove('lumberjack');
         lumberjackIndex += 1;
+        console.log(lumberjackIndex);
         cells[lumberjackIndex].classList.add('lumberjack');
       }
+      checkBear();
     }
     if (e.keyCode === 40) {
-      console.log('down');
       if (!(lumberjackIndex > (gridWidth * gridHeight) - gridWidth) && !cells[lumberjackIndex+gridWidth].classList.contains('tree')){
         cells[lumberjackIndex].classList.remove('lumberjack');
         lumberjackIndex += gridWidth;
         cells[lumberjackIndex].classList.add('lumberjack');
       }
+      checkBear();
     }
     if (e.keyCode === 38) {
       console.log('up');
@@ -89,6 +107,7 @@ window.addEventListener('DOMContentLoaded', () => {
         lumberjackIndex -= gridWidth;
         cells[lumberjackIndex].classList.add('lumberjack');
       }
+      checkBear();
     }
 
     if (e.keyCode === 32){
@@ -96,33 +115,34 @@ window.addEventListener('DOMContentLoaded', () => {
       if (inventory > 0){
         inventory--;
         console.log(inventory);
-        const pineconeIndex = cells[lumberjackIndex];
-        pineconeIndex.classList.add('pinecone');
+        // const lumberjackAttack = cells[lumberjackIndex];
+        lumberjackIndex.classList.add('lumberjackAttack');
         window.setTimeout(() => {
-          pineconeIndex.classList.remove('pinecone');
-        } ,2000);
+          lumberjackIndex.classList.remove('lumberjackAttack');
+        } ,500);
       }
     }
   }, false);
 
   //SPAWN RANDOM ITEMS
-
   function spawnItems(){
-  //set timeout?
-    pineconeIndex = Math.floor(Math.random() * (gridHeight*gridWidth));
-    while(trees.includes(pineconeIndex) &&  bearIndex.includes(pineconeIndex)){
+    window.setTimeout(() => {
       pineconeIndex = Math.floor(Math.random() * (gridHeight*gridWidth));
-    }
-    cells[pineconeIndex].classList.add('pinecone');
+      while(trees.includes(pineconeIndex)){
+        pineconeIndex = Math.floor(Math.random() * (gridHeight*gridWidth));
+      }
+      cells[pineconeIndex].classList.add('pinecone');
+    }, 1000);
+    //work out how not to get it in tree squares
+    //convert to jquery with addClass later
   }
-  //work out how not to get it in tree squares
-  //convert to jquery with addClass later
-  // }
   spawnItems();
 
-  // BEAR MOVEMENTS
   const bearPosition = ['up', 'down', 'left', 'right'];
   let direction = bearPosition[0];
+  // BEAR MOVEMENTS
+  // bearMovements();
+  // function bearMovements(){
   window.setInterval(() => {
     switch(direction) {
       case 'right':
@@ -135,6 +155,7 @@ window.addEventListener('DOMContentLoaded', () => {
           direction = bearPosition[Math.floor(Math.random() * bearPosition.length)];
           console.log(direction);
         }
+        checkBear();
         break;
       case 'left':
         if(bearIndex%gridWidth !== 0 && !cells[bearIndex-1].classList.contains('tree')) {
@@ -146,6 +167,7 @@ window.addEventListener('DOMContentLoaded', () => {
           direction = bearPosition[Math.floor(Math.random() * bearPosition.length)];
           console.log(direction);
         }
+        checkBear();
         break;
       case 'down':
         if(bearIndex < gridWidth*gridHeight-gridWidth && !cells[bearIndex+gridWidth].classList.contains('tree')) {
@@ -157,6 +179,7 @@ window.addEventListener('DOMContentLoaded', () => {
           direction = bearPosition[Math.floor(Math.random() * bearPosition.length)];
           console.log(direction);
         }
+        checkBear();
         break;
       case 'up':
         if(bearIndex > gridWidth-1 && !cells[bearIndex-gridWidth].classList.contains('tree')) {
@@ -168,10 +191,9 @@ window.addEventListener('DOMContentLoaded', () => {
           direction = bearPosition[Math.floor(Math.random() * bearPosition.length)];
           console.log(direction);
         }
+        checkBear();
         break;
     }
   }, 2000);
-
-
-
+  // }
 });
