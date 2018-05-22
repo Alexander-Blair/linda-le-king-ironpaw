e011311bf2a93524135b087f56dd020bfb74471d
Linda Le <linchowmein@hotmail.com>
2018-05-22 15:23:19 +0100

    pressing spacebar and moving before the action has timed out will no longer leave behind a lumberjack and added functional life bar

--- js/app.js
+++ js/app.js
@@ -1,11 +1,13 @@
 window.addEventListener('DOMContentLoaded', () => {
   // To do list
   //instructions button
-  //health bar
-  //timer
-  //turn into smooth based tile game
+  //timer?
+  //turn into smooth based tile game?
   //add score
-  // add logic
+  //add logic
+  //pinecone and lumberjack - set inline two backgrounds at once. set seperate class for pinecone and lumberjack
+  //set lumberjack state
+  //create class for pinecone thrown vs pincone
 
 
   //GLOBAL VARIABLES
@@ -13,21 +15,48 @@ window.addEventListener('DOMContentLoaded', () => {
   const gridHeight = 10;
   const gridWidth = 10;
   const cells = [];
-  const trees = [7,8,9,20,21,22,34,38,44,48,54,61,67,71,77,81,87];
+
+  // Dom variables
   const grid = document.querySelector('#grid');
-  const p1life = document.querySelector('#lifebar');
+  const messageBox = document.querySelector('.messageBox');
+  //make these
   // const p2life = document.querySelector('#lifebar');
-  const messageDisplay = document.querySelector('.message');//make display
+  // const newGame = document.querySelector('#newGame');
+  // const instructionsBtn = document.querySelector('.instructionsBtn')
+  // const timerDisplay = document.querySelector('.timerDisplay')
+  // const messageDisplay = document.querySelector('.message');//make display
+  let className = 'lumberjack';
   let lumberjackIndex = 0;
   let bearIndex = 99;
   let pineconeIndex = null;
   let inventory = 5;
   let score = 10;
-  let life =3;
+  let numOfLives =3;
+  let lifebar;
+  let lumberjackState = 0;
+  let actionCell;
+  let spacebarHeld = true;
   // let computerPlayer =false;
   //let hit = true; If attacks hit
 
-  // 1. create grid
+
+  //LIBRARY
+  const trees = [7,8,9,20,21,22,34,38,44,48,54,61,67,71,77,81,87];
+  // class characterSpecs =[{
+
+  //
+  // }]
+
+
+  //Gameplay Variables
+  // const moveKeys ={
+  //   right: 1,
+  //   left: -1,
+  //   down: gridHeight,
+  //   up: -gridHeight
+  // };
+
+  // CREATES GRID
   //Rendering static tilemaps done with a nested loop iterating over columns and rows.
   function startGame(){
     for (let i=0; i<gridHeight*gridWidth; i++) {
@@ -37,10 +66,21 @@ window.addEventListener('DOMContentLoaded', () => {
       cells.push(div);
     }
   }
-
-  //pro
   startGame();
 
+  function init(){
+    lifebar = document.querySelector('#lifebar');
+    for(let i=0; i<3; i++) addLife();
+  }
+  init();
+
+
+  function addLife(){
+    numOfLives++;
+    const life = document.createElement('img');
+    life.src ='./images/heart.png';
+    lifebar.appendChild(life);
+  }
   //add lumberjackIndex to grid
   cells[lumberjackIndex].classList.add('lumberjack');
 
@@ -58,18 +98,51 @@ window.addEventListener('DOMContentLoaded', () => {
   function checkBear() {
     if(lumberjackIndex === bearIndex){
       score--;
-      life--;
-      console.log(score);
-      cells[lumberjackIndex].classList.add('lumberjackHurt');
-      // cells[bearIndex].classList.add('bearAttack');
-      window.setTimeout(() => {
-        cells[lumberjackIndex].classList.remove('lumberjackHurt');
-        // cells[bearIndex].classList.remove('bearAttack');
-      } ,500);
+      numOfLives --;
+      const lifebar = document.querySelector('#lifebar')
+      if(lifebar.lastChild) lifebar.removeChild(lifebar.lastChild);
+      else lumberjackState = 3; // dead!
+      actionCell = cells[lumberjackIndex];
+      actionCell.classList.add('lumberjackAttack');
+    } else {
     }
   }
 
+  if(numOfLives < 1){
+    console.log('You dead');
+    // loseGame();
+  }
+
+  window.setInterval(function() {
+    switch(lumberjackState) {
+      case 0: //normal
+        if(actionCell) actionCell.classList.remove('lumberjackAttack');
+        className = 'lumberjack';
+        break;
+      case 1: // if attacking
+        if(actionCell) actionCell.classList.remove('lumberjackAttack');
+        actionCell = cells[lumberjackIndex];
+        className = 'lumberjackAttack';
+        break;
+      case 2: //if hurt
+        actionCell = cells[lumberjackIndex];
+        className = 'lumberjackHurt';
+        score --;
+        numOfLives --;
+        break;
+      case 3:
+        console.log('dead!!!');
+        //write end game
+    }
+    // show the lumberjack in the right place
+    cells[lumberjackIndex].classList.add(className);
+  }, 50);
+
   //ARROW BINDING
+
+  window.addEventListener('keyup', (e) => {
+    lumberjackState = 0;
+  });
   window.addEventListener('keydown', (e) => {
 
     //picking up pinecone and respawn
@@ -120,15 +193,17 @@ window.addEventListener('DOMContentLoaded', () => {
     }
 
     if (e.keyCode === 32){
+      lumberjackState = 1; // attacking!
       console.log('spacebar');
       if (inventory > 0){
         inventory--;
         console.log(inventory);
-        // const lumberjackAttack = cells[lumberjackIndex];
-        cells[lumberjackIndex].classList.add('lumberjackAttack');
-        window.setTimeout(() => {
-          cells[lumberjackIndex].classList.remove('lumberjackAttack');
-        } ,500);
+        // // const lumberjackAttack = cells[lumberjackIndex];
+        // cells[lumberjackIndex].classList.add('lumberjackAttack');
+        // window.setTimeout(() => {
+        //   lumberjackIndex = 0;
+        //   cells[lumberjackIndex].classList.remove('lumberjackAttack');
+        // } ,500);
       }
     }
   }, false);
@@ -143,15 +218,13 @@ window.addEventListener('DOMContentLoaded', () => {
       cells[pineconeIndex].classList.add('pinecone');
     }, 1000);
     //work out how not to get it in tree squares
-    //convert to jquery with addClass later
+    //Add beer to add life
   }
   spawnItems();
 
+  // BEAR MOVEMENTS
   const bearPosition = ['up', 'down', 'left', 'right'];
   let direction = bearPosition[0];
-  // BEAR MOVEMENTS
-  // bearMovements();
-  // function bearMovements(){
   window.setInterval(() => {
     switch(direction) {
       case 'right':
@@ -200,5 +273,4 @@ window.addEventListener('DOMContentLoaded', () => {
         break;
     }
   }, 1000);
-  // }
 });
