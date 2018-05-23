36074e29b63209384c9a1ebbac143ba795091aa4
Linda Le <linchowmein@hotmail.com>
2018-05-23 18:04:31 +0100

    pinecones now throw from two squares away. Changed keys to IJKL to fire in direction to add another level to the game's complexity. Added some HTML to begin styling

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const gridHeight = 10;
   const gridWidth = 10;
   const gridSize = gridHeight * gridWidth;
-  const cells = [];
+  let cells = [];
 
   // Dom variables
   const grid = document.querySelector('#grid');
@@ -37,6 +37,7 @@ document.addEventListener('DOMContentLoaded', () => {
   // const instructionsBtn = document.querySelector('.instructionsBtn')
   // const messageDisplay = document.querySelector('.message');//make display
   let className = 'lumberjack';
+  let bearClassName = 'bear';
   let lumberjackIndex = 0;
   let bearIndex = 99;
   let pineconeIndex = null;
@@ -45,9 +46,9 @@ document.addEventListener('DOMContentLoaded', () => {
   let numOfLives = 3;
   let lifebar;
   let lumberjackState = 0;
+  let bearState =0;
   let actionCell;
-  let pineconeBtn = false;
-  // let bearIsHit = false;  //if true clearInterval
+
 
 
   // //Event listeners
@@ -91,6 +92,11 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   init();
 
+  //add lumberjackIndex to grid
+  cells[lumberjackIndex].classList.add('lumberjack');
+
+  //add bear to grid
+  cells[bearIndex].classList.add('bear');
 
 
   let tick = 60;
@@ -109,13 +115,6 @@ document.addEventListener('DOMContentLoaded', () => {
     lifebar.appendChild(life);
   }
 
-  //add lumberjackIndex to grid
-  cells[lumberjackIndex].classList.add('lumberjack');
-
-  //add bear to grid
-  cells[bearIndex].classList.add('bear');
-
-
   function generateTrees(){
     for (let i=0; i<trees.length; i++) {
       cells[trees[i]].classList.add('tree');
@@ -123,21 +122,53 @@ document.addEventListener('DOMContentLoaded', () => {
   }
   generateTrees();
 
+  //LOSE THE GAME
+  function loseGame(){
+    cells =[];
+    grid.innerHTML ='';
+  }
+
+
+  //checks if cell has bear
   function checkBear() {
     if(lumberjackIndex === bearIndex){
       score--;
       numOfLives --;
       lumberjackState =2;
-      actionCell = cells[lumberjackIndex];
-      actionCell.classList.add('lumberjackHurt');
 
       if(lifebar.lastChild) lifebar.removeChild(lifebar.lastChild);
       else lumberjackState = 3; // dead!
-      actionCell = cells[lumberjackIndex];
-      actionCell.classList.add('lumberjackAttack');
     }
   }
 
+  //BEAR'S STATE
+  window.setInterval(function() {
+    switch(bearState) {
+      case 0: //normal
+        if(actionCell) actionCell.classList.remove('bearAttack', 'bearHurt');
+        actionCell = cells[bearIndex];
+        bearClassName = 'bear';
+        break;
+      case 1: // if attacking
+        if(actionCell) actionCell.classList.remove('bearAttack');
+        actionCell = cells[bearIndex];
+        bearClassName = 'bearAttack';
+        break;
+      case 2: //if hurt
+        if(actionCell) actionCell.classList.remove('bearHurt');
+        actionCell = cells[bearIndex];
+        bearClassName = 'bearHurt';
+        score ++;
+        break;
+    }
+    // show the bear in the right place
+    if(cells[bearIndex])
+      cells[bearIndex].classList.add(bearClassName);
+  }, 50);
+
+
+
+  //LUMBERJACK STATE
   window.setInterval(function() {
     switch(lumberjackState) {
       case 0: //normal
@@ -159,22 +190,19 @@ document.addEventListener('DOMContentLoaded', () => {
         break;
       case 3:
       //dead!
-      //write losegame function;
+        loseGame();
     }
     // show the lumberjack in the right place
-    cells[lumberjackIndex].classList.add(className);
+    if(cells[lumberjackIndex])
+      cells[lumberjackIndex].classList.add(className);
   }, 50);
 
   //ARROW BINDING
   document.addEventListener('keyup', (e) => {
     lumberjackState = 0;
-    pineconeBtn = false;
   });
   document.addEventListener('keydown', (e) => {
     const code =e.keyCode;
-    const lumberjackPosition = ['up','down','left', 'right'];
-    const pineconeDirection = lumberjackPosition[0];
-    // delay =300;
 
     //picking up pinecone and respawn
     if (cells[lumberjackIndex].classList.contains('pinecone')){
@@ -186,7 +214,8 @@ document.addEventListener('DOMContentLoaded', () => {
         console.log(inventory);
       }
     }
-    if (code === 65) {
+    //PLAYER MOVEMENTS
+    if (code === 65){
       //left
       if(lumberjackIndex % gridWidth !== 0 && !cells[lumberjackIndex-1].classList.contains('tree')){
         cells[lumberjackIndex].classList.remove('lumberjack');
@@ -206,7 +235,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     if (code === 83) {
       //down
-      if (!(lumberjackIndex > (gridWidth * gridHeight) - gridWidth) && !cells[lumberjackIndex+gridWidth].classList.contains('tree')){
+      if (!(lumberjackIndex > (gridWidth * gridHeight) - gridWidth) && cells[lumberjackIndex+gridWidth] && !cells[lumberjackIndex+gridWidth].classList.contains('tree')){
         cells[lumberjackIndex].classList.remove('lumberjack');
         lumberjackIndex += gridWidth;
         cells[lumberjackIndex].classList.add('lumberjack');
@@ -223,35 +252,37 @@ document.addEventListener('DOMContentLoaded', () => {
       checkBear();
     }
 
-    if(code === 80 || code === 16){
-      // p = 80
-      // shift = 16
-      pineconeBtn = true;
-      console.log('p is for pinecone!', inventory);
+    // PINECONE FIRING
+    if(code === 76){//right attack
       lumberjackState = 1; // attacking!
+      if (bearIndex === lumberjackIndex +1 || bearIndex === lumberjackIndex + 2){
+        bearState =2;
+        score++;
+        console.log('pinecone!', inventory, score);
+      }
     }
-    if(pineconeBtn === true){
-      switch(pineconeDirection){
-        case 'right':
-          if(code === 68 && !cells[lumberjackIndex+1].classList.contains('tree')){
-            cells[lumberjackIndex+1].classList.add('pineconeAmmo');
-          }
-          break;
-        case 'left':
-          if(code === 68 && !cells[lumberjackIndex-1].classList.contains('tree')){
-            cells[lumberjackIndex-1].classList.add('pineconeAmmo');
-          }
-          break;
-        case 'down':
-          if(code === 68 && !cells[lumberjackIndex + gridWidth].classList.contains('tree')){
-            cells[lumberjackIndex + gridWidth].classList.add('pineconeAmmo');
-          }
-          break;
-        case 'up':
-          if(code === 68 && !cells[lumberjackIndex - gridWidth].classList.contains('tree')){
-            cells[lumberjackIndex -gridWidth].classList.add('pineconeAmmo');
-          }
-          break;
+    if(code === 74){//left attack
+      lumberjackState = 1; // attacking!
+      if (bearIndex === lumberjackIndex - 1 || bearIndex === lumberjackIndex - 2){
+        bearState =2;
+        score++;
+        console.log('pinecone!', inventory, score);
+      }
+    }
+    if(code === 75){//down attack
+      lumberjackState = 1; // attacking!
+      if (bearIndex === lumberjackIndex + gridWidth || bearIndex === lumberjackIndex +(gridWidth* 2)){
+        bearState =2;
+        score++;
+        console.log('pinecone!', inventory, score);
+      }
+    }
+    if(code === 73){ //up attack
+      lumberjackState = 1; // attacking!
+      if (bearIndex === lumberjackIndex - gridWidth || bearIndex === lumberjackIndex -(gridWidth* 2)){
+        bearState =2;
+        score++;
+        console.log('pinecone!', inventory, score);
       }
     }
   }, false);
@@ -273,10 +304,11 @@ document.addEventListener('DOMContentLoaded', () => {
   const bearPosition = ['up', 'down', 'left', 'right'];
   let direction = bearPosition[0];
   window.setInterval(() => {
+    bearState =0;
     switch(direction) {
       case 'right':
         if(bearIndex % gridWidth !== gridWidth-1 && !cells[bearIndex+1].classList.contains('tree')) {
-          cells[bearIndex].classList.remove('bear');
+          cells[bearIndex].classList.remove('bear', 'bearHurt', 'bearAttack');
           bearIndex += 1;
           cells[bearIndex].classList.add('bear');
         } else {
@@ -287,7 +319,7 @@ document.addEventListener('DOMContentLoaded', () => {
         break;
       case 'left':
         if(bearIndex % gridWidth !== 0 && !cells[bearIndex-1].classList.contains('tree')) {
-          cells[bearIndex].classList.remove('bear');
+          cells[bearIndex].classList.remove('bear', 'bearAttack', 'bearHurt');
           bearIndex -= 1;
           cells[bearIndex].classList.add('bear');
         } else {
@@ -298,7 +330,7 @@ document.addEventListener('DOMContentLoaded', () => {
         break;
       case 'down':
         if(bearIndex < gridWidth*gridHeight-gridWidth && !cells[bearIndex+gridWidth].classList.contains('tree')) {
-          cells[bearIndex].classList.remove('bear');
+          cells[bearIndex].classList.remove('bear', 'bearAttack', 'bearHurt');
           bearIndex += gridWidth;
           cells[bearIndex].classList.add('bear');
         } else {
@@ -309,7 +341,7 @@ document.addEventListener('DOMContentLoaded', () => {
         break;
       case 'up':
         if(bearIndex > gridWidth-1 && !cells[bearIndex-gridWidth].classList.contains('tree')) {
-          cells[bearIndex].classList.remove('bear');
+          cells[bearIndex].classList.remove('bear', 'bearAttack', 'bearHurt');
           bearIndex -= gridWidth;
           cells[bearIndex].classList.add('bear');
         } else {
@@ -320,4 +352,27 @@ document.addEventListener('DOMContentLoaded', () => {
         break;
     }
   }, 1000);
+
+
+
+  // function updatePosition(index, direction) {
+  //   // depending on direction, change index
+  //   switch (direction){
+  //     case 'up':
+  //       index - gridWidth;
+  //       break;
+  //     case 'down':
+  //       index + gridWidth;
+  //       break;
+  //     case 'left':
+  //       index -1;
+  //       break;
+  //     case 'right':
+  //       index + 1;
+  //       break;
+  //   }
+  //   return index;
+  // }
+
+
 });
